10e326387437fcb715a128f469754298c738963a
LGregg11 <L.Gregory11@hotmail.co.uk>
2021-06-11 21:55:55 +0100

    Add LeagueOfLegends Bot

---
+++ Python/LeagueOfLegendsBot/LeagueOfLegendsBot.py
@@ -0,0 +1,55 @@
+from Python.DiscordBotClient import DiscordBotClient
+from Python.LeagueOfLegendsBot.LeagueOfLegendsWrapper import LolWrapper
+import pathlib
+
+CONFIGS = {
+    "bot_name": "LeagueOfLegendsBot",
+    "file_path": pathlib.Path(__file__).parent.absolute(),
+    "command_prefix": "-"
+}
+
+
+class LeagueOfLegendsBot(DiscordBotClient):
+    """
+    A Discord bot to return instant information and details on players and matches
+    """
+
+    def __init__(self, command_prefix=None, debug=False):
+        super().__init__(
+            CONFIGS["bot_name"],
+            CONFIGS["file_path"],
+            CONFIGS["command_prefix"] if not command_prefix else command_prefix,
+            debug
+        )
+        self.lol_wrapper = LolWrapper()
+        self.commands = {
+            "help": self.get_help,
+            "rank": self.get_summoner_rank,
+        }
+
+    async def on_message(self, ctx):
+        if not ctx.content.startswith(self.command_prefix):
+            return
+
+        full_command = ctx.content[len(self.command_prefix):]
+
+        if " " in full_command:
+            command = full_command.split(" ")[0]
+            args = full_command.split(" ")[1:]
+        else:
+            command = full_command
+            args = []
+
+        func = self.commands.get(command.lower(), lambda: f"Invalid command\n" +
+                                                          "Type {self.command_prefix}help for list of commands!")
+        await ctx.channel.send(func(*args))
+
+    def get_help(self):
+        join_str = f"\n\t{self.command_prefix}"
+        return f"```list of commands:{join_str}" + join_str.join(self.commands.keys()) + "```"
+
+    def get_summoner_rank(self, *args):
+        rank_list = []
+        for summoner_name in args:
+            rank_list.append(self.lol_wrapper.get_summoner_rank_by_name(summoner_name))
+        return "\n".join(rank_list)
---
+++ Python/LeagueOfLegendsBot/README.md
@@ -0,0 +1,16 @@
+# League of Legends Bot
+
+## Ideas for the bot
+1. Ask for player rank and points
+2. Ask for general player info
+3. Ask for most played champs etc
+4. Ask for player win rate
+
+### How to develop locally
+1. Create a 'LeagueOfLegendsAPI.json' file
+5. Store the LOL API details in the LeagueOfLegendsAPI.json file as follows:
+{
+    "APIKEY": \<ApiKey\>
+}
+
+Note: Don't commit this json file..!
