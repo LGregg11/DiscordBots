aebae238f7bd3a2ac50fa3aab8bd82ac123849b4
LGregg11 <L.Gregory11@hotmail.co.uk>
2021-06-14 01:26:01 +0100

    LolBot: Update get_summoner_rank to display additional rank data

--- Python/LeagueOfLegendsBot/LeagueOfLegendsBot.py
+++ Python/LeagueOfLegendsBot/LeagueOfLegendsBot.py
@@ -25,6 +25,32 @@ class LeagueOfLegendsBot(DiscordBotClient):
             CONFIGS["command_prefix"] if not command_prefix else command_prefix,
             debug
         )
+        self.discord_emotes |= {
+            "veteran": ":older_adult:",
+            "blood": ":drop_of_blood:",
+            "inactive": ":sleeping:",
+            "W": ":white_check_mark:",
+            "L": ":x:",
+            "N": ":shrug:"
+        }
+
+        self.ranks = {
+            "IRON": "Wood",
+            "SILVER": "Silver",
+            "GOLD": "Gold",
+            "PLATINUM": "Plat",
+            "DIAMOND": "Diamond",
+            "MASTER": "Master",
+            "GRANDMASTER": "GM",
+            "CHALLENGER": "Challenger"
+        }
+        self.divisions = {
+            "I": "1",
+            "II": "2",
+            "III": "3",
+            "IV": "4"
+        }
+
         self.lol_api_wrapper = LolAPIWrapper()
         self.commands = {
             "help": {
@@ -82,17 +108,32 @@ class LeagueOfLegendsBot(DiscordBotClient):
         :param summoner_names: list of summoner names to check each summoner's rank details.
         """
 
-        import json
-
+        summoner_rank_strings = []
         for summoner_name in summoner_names:
             try:
                 summoner_rank_json = self.lol_api_wrapper.get_summoner_rank_by_name(summoner_name)
-                print(json.dumps(summoner_rank_json, indent=4, sort_keys=True))
-                if not summoner_rank_json or len(summoner_rank_json) == 0:
-                    summoner_rank = f"{summoner_name} does not have a rank"
-                else:
-                    summoner_rank_json = summoner_rank_json[0]
-                    summoner_rank = f"{summoner_name} is in {summoner_rank_json['tier']} {summoner_rank_json['rank']}"
-                await channel.send(summoner_rank)
+                summoner_rank_strings.append(self.get_specific_rank_details(summoner_name, summoner_rank_json[0]))
             except Exception as ex:
-                await channel.send(str(ex))
+                summoner_rank_strings.append(str(ex))
+        await channel.send("\n".join(summoner_rank_strings))
+
+    def get_specific_rank_details(self, summoner_name, rank_dict):
+        if not rank_dict or len(rank_dict) == 0:
+            return f"{summoner_name} - no rank"
+        s = f"{summoner_name} - {self.ranks[rank_dict['tier']]} {self.divisions[rank_dict['rank']]}, {rank_dict['leaguePoints']}lp"
+
+        if "miniSeries" in rank_dict.keys():
+            progress = rank_dict["miniSeries"]["progress"].replace(
+                "W", self.discord_emotes["W"]).replace(
+                "L", self.discord_emotes["L"]).replace(
+                "N", self.discord_emotes["N"])
+            s += f" (promos: {progress})"
+        if "hotStreak" in rank_dict.keys() and rank_dict["hotStreak"]:
+            s += f" {self.discord_emotes['fire']}"
+        if "veteran" in rank_dict.keys() and rank_dict["veteran"]:
+            s += f" {self.discord_emotes['veteran']}"
+        if "freshBlood" in rank_dict.keys() and rank_dict["freshBlood"]:
+            s += f" {self.discord_emotes['blood']}"
+        if "inactive" in rank_dict.keys() and rank_dict["inactive"]:
+            s += f" {self.discord_emotes['inactive']}"
+        return s
